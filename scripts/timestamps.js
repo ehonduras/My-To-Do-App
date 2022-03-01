6be86197e796d24d65c819e271485a65469e77de
ldrljaca <ermin.h@protonmail.com>
2022-03-01 06:12:21 +0100

    dv

--- scripts/timestamps.js
+++ scripts/timestamps.js
@@ -1,7 +1,7 @@
 let allNotes = JSON.parse(localStorage.getItem('createdNotes'));
 
 let filteredArray = allNotes.filter(checkNotesForDeletion);
-
+localStorage.setItem('notes', JSON.stringify(filteredArray));
 
 function checkNotesForDeletion(note) {
     if ((Date.now().getTime() - Date(note.noteDate).getTime()) < 172800000) {
